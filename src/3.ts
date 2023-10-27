bb878c5ba6be4792d3365de7048983849d6a021b
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-10-28 00:52:08 +0300

    task 3 done

--- src/3.ts
+++ src/3.ts
@@ -45,7 +45,6 @@ const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
 wizard.levelUp(); // Level up! New level is 16
 
 export {};
